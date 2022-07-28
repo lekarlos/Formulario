fc2f21f5cfb8dd60ef11f779346c661103223448
lekarlos <105306612+lekarlos@users.noreply.github.com>
2022-07-28 10:52:22 -0500

    Add files via upload

---
+++ ZRLE.php
@@ -0,0 +1,79 @@
+<?php require_once 'controllers_motivos/c3.php'; ?>
+<!DOCTYPE html>
+    <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" id="hello-elementor-css" href="https://pruebabre.azurefd.net/wp-content/themes/hello-elementor/style.min.css?ver=2.5.0" type="text/css" media="all">
+<link rel="stylesheet" id="elementor-post-24795-css" href="https://pruebabre.azurefd.net/wp-content/uploads/elementor/css/post-24795.css?ver=1656955234" type="text/css" media="all">
+<link rel="stylesheet" id="hello-elementor-child-style-css" href="https://pruebabre.azurefd.net/wp-content/themes/hello-theme-child-master/style.css?ver=1.0.0" type="text/css" media="all">
+
+     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+     <title>Formulario_ZRLE</title>
+    </head>
+    
+    <body>
+    
+        
+                
+
+
+              <?php if(count($errors)>0):?>
+                <div class="alert alert-danger">
+                <?php foreach($errors as $error):?>
+                <li><?php echo $error; ?></li>
+                <?php endforeach; ?>
+                  </div>
+                  <?php endif; ?>
+                
+                  <?php if(count($Ticket)>0):?>
+                <div class="alert alert-success" role="alert">
+                <?php foreach($Ticket as $Ticket):?>
+                <li><?php echo $Ticket; ?></li>
+                <?php endforeach; ?>
+                  </div>
+                  <?php endif; ?>
+
+
+<section class="elementor-section elementor-top-section elementor-element elementor-element-d5947a3 elementor-section-boxed elementor-section-height-default elementor-section-height-default" data-id="d5947a3" data-element_type="section" data-settings="{&quot;_ha_eqh_enable&quot;:false}">
+<div class="elementor-container elementor-column-gap-default">
+<div class="elementor-column elementor-col-100 elementor-top-column elementor-element elementor-element-6ffaf85" data-id="6ffaf85" data-element_type="column">
+<div class="elementor-widget-wrap elementor-element-populated">
+<div class="elementor-element elementor-element-cda6c3d elementor-widget elementor-widget-shortcode" data-id="cda6c3d" data-element_type="widget" data-widget_type="shortcode.default">
+<div class="elementor-widget-container">
+<div class="elementor-shortcode"><div role="form" class="wpcf7" id="wpcf7-f24659-p24660-o1" lang="en-US" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>
+
+<form action="ZRLE.php" method="post">
+  <p><label> Motivo de tu comunicación<br>
+  <span class="wpcf7-form-control-wrap" data-name="Motivodetucomunicacin">
+  <select name="Motivodetucomunicacin" class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required" aria-required="true" aria-invalid="false"><option value="">---</option>
+  <option value="Información">Información</option>
+  <option value="Sugerencia">Sugerencia</option>
+  <option value="Queja">Queja</option>
+  <option value="Comentario positivo">Comentario positivo</option></select></span><br>
+  </label></p>
+	<p><label> Nombre <span>*</span><br>
+		<span class="wpcf7-form-control-wrap" data-name="nombre"><input type="text" name="nombre" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span> </label></p>
+	<p><label> Primer apellido <span>*</span><br>
+		<span class="wpcf7-form-control-wrap" data-name="apellido1"><input type="text" name="apellido1" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span> </label></p>
+	<p><label> Segundo apellido <span>*</span><br>
+		<span class="wpcf7-form-control-wrap" data-name="apellido2"><input type="text" name="apellido2" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span> </label></p>
+	<p><label> Email <span>*</span><br>
+		<span class="wpcf7-form-control-wrap" data-name="email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span> </label></p>
+	<p><label> Asunto <span>*</span><br>
+		<span class="wpcf7-form-control-wrap" data-name="asunto"><textarea name="asunto" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea" aria-invalid="false"></textarea></span> </label></p>
+	<p><span class="wpcf7-form-control-wrap" data-name="acceptance-636"><span class="wpcf7-form-control wpcf7-acceptance optional"><span class="wpcf7-list-item"><label><input type="checkbox" name="acceptance-636" value="1" aria-invalid="false"><span class="wpcf7-list-item-label">He leído y acepto el <a href="http://cem.westus.cloudapp.azure.com/avisos-de-privacidad/"> Aviso de Privacidad </a></span></label></span></span></span> </p>
+	<p><input type="submit" name="formu_zrle" value="Enviar formulario" class="wpcf7-form-control has-spinner wpcf7-submit"><span class="wpcf7-spinner"></span></p>
+	<div class="wpcf7-response-output" aria-hidden="true"></div>
+
+
+  </form>
+</div>
+</div>
+</div>
+          
+          
+    </body>
+    </html>
---
+++ c4.php
@@ -0,0 +1,212 @@
+<?php
+
+
+$errors = array();
+$patron_texto = "/^[a-zA-ZáéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙ\s]+$/";
+$patron_correo="/^[a-zA-Z0-9._áéíóúÁÉÍÓÚäëïöüÄËÏÖÜàèìòùÀÈÌÒÙ\s]+[@]+[a-zA-Z]+[.]+[a-zA-Z]+$/";
+$aMensajes = array();
+$Ticket = array();
+$nombre="";
+$email ="";
+
+
+
+
+if (isset($_POST['formu_zrvb'])) {
+   $nombre = $_POST['nombre'];
+   $email = $_POST['email'];
+   $apellido1 = $_POST['apellido1'];
+   $apellido2 = $_POST['apellido2'];
+
+   // validation
+   if( empty($_POST['Motivodetucomunicacin']) ){
+      $errors[] = "Debe especificar el Motivo";
+   
+    }
+          
+      else
+      {
+          // Comprobar mediante una expresión regular, que sólo contiene letras y espacios:
+           if( preg_match($patron_texto, $_POST['Motivodetucomunicacin']) ){
+         $aMensajes[] = "Motivodetucomunicacin: [".$_POST['Motivodetucomunicacin']."]";
+         
+       }else{
+         $errors[] = "El motivo sólo puede contener letras y espacios";
+         
+       }
+              
+   }
+      // nombre
+       if( empty($_POST['nombre']) ){
+      $errors[] = "Debe especificar el nombre";
+      
+    }
+          
+      else
+      {
+          // Comprobar mediante una expresión regular, que sólo contiene letras y espacios:
+           if( preg_match($patron_texto, $_POST['nombre']) ){
+         $aMensajes[] = "Nombre: [".$_POST['nombre']."]";
+         
+       }else{
+         $errors[] = "El nombre sólo puede contener letras y espacios";
+         
+       }
+              
+   }
+   //apellido1
+   if( empty($_POST['apellido1']) )
+   $errors[] = "Debe especificar apellido paterno";
+else
+{
+   // Comprobar mediante una expresión regular, que sólo contienen letras y espacios:
+   if( preg_match($patron_texto, $_POST['apellido1']) )
+      $aMensajes[] = "Apellidos: [".$_POST['apellido1']."]";
+   else
+      $errors[] = "Los apellidos sólo pueden contener letras y espacios";
+}
+
+   //apellido2
+   if( empty($_POST['apellido2']) )
+   $errors[] = "Debe especificar apellido materno";
+else
+{
+   // Comprobar mediante una expresión regular, que sólo contienen letras y espacios:
+   if( preg_match($patron_texto, $_POST['apellido2']) )
+      $aMensajes[] = "Apellidos: [".$_POST['apellido2']."]";
+   else
+      $errors[] = "Los apellidos sólo pueden contener letras y espacios";
+}
+//correo
+   
+   if( empty($_POST['email']) )
+   $errors[] = "Debe especificar el email";
+else
+{
+   // Comprobar mediante una expresión regular, que sólo contienen letras y espacios:
+   if (filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+      $aMensajes[] = "Correo: [".$_POST['email']."]";
+   }
+   else{
+      $errors[] = "El correo no esta redactado bien";
+   }
+}
+//asunto
+if( empty($_POST['asunto']) ){
+   $errors[] = "Debe especificar el asunto";
+   
+ }
+   
+else
+{
+   // Comprobar mediante una expresión regular, que sólo contiene letras y espacios:
+    if( preg_match($patron_texto, $_POST['asunto']) ){
+      $aMensajes[] = "Asunto: [".$_POST['asunto']."]";
+      
+    }else{
+      $errors[] = "El asunto sólo puede contener letras y espacios";
+      
+    }
+      
+}
+if( empty($_POST['acceptance-636']) ){
+   $errors[] = "Debe aceptar los terminos y condiciones";
+   
+ }
+     if (count($errors) === 0) {
+     //sin errores
+     $Ticket[] = "Tu ticket es ".peticion()." en breve lo atenderemos";
+     //peticion();
+
+     }
+
+   }
+
+
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function peticion(){
+    $Motivo=$_POST['Motivodetucomunicacin'];
+    $Ma="";//Motivo actualizado
+    switch($Motivo){
+        case "Información":
+            $Ma="VU-IN";
+            break;
+        case "Sugerencia":
+            $Ma="VU-SUG";
+            break;
+        case "Queja":
+            $Ma="VU-QU";
+            break;
+        case "Comentario positivo":
+            $Ma="VU-COM";
+            break;
+    }
+	$datos=array(
+		'Motivo_comunicacion' => $Ma,
+		'Nombre'   => htmlentities($_POST['nombre']),
+		'Apellido_paterno' => htmlentities($_POST['apellido1']),
+		'Apellido_materno' => htmlentities($_POST['apellido2']),
+		'Email'    => htmlentities($_POST['email']),
+		'Asunto' => $_POST['asunto'],
+		'Unidad_negocio' => 'ZRVB',
+	);
+	$username = 'S0023952345';
+	$password = 'Septiembre.2021';
+	
+	$site_url = 'https://televia.it-cpi003-rt.cfapps.us10.hana.ondemand.com/http/ServiceTicketCreateAPI';
+	$json = json_encode($datos);
+	
+	$keypair = $username . ":" . $password;
+	$curl=curl_init();
+	$header = array('Content-Type: application/json','Content-Length: ' . strlen($json));
+
+	curl_setopt($curl,CURLOPT_URL,$site_url);
+	curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+	curl_setopt($curl, CURLOPT_POST, 1);
+	curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+	curl_setopt($curl, CURLOPT_USERPWD, $keypair);
+	curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+	curl_setopt($curl, CURLOPT_HTTPHEADER, $header);
+	
+	
+	$response = curl_exec($curl);
+	$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+	$obj = json_decode($json);
+	
+	curl_close($curl);
+
+	$texto=json_decode($response, true);
+	
+	
+
+	$tam=strlen($response);
+	$final=strpos($response,"<d:TicketID>");
+	
+
+   return substr($response,$final+12,5);
+	
+   
+	  
+	// Function call
+	//function_alert(substr($response,$final+12,4));
+}
+	
+	
+	
+
+	
